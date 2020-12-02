5ffe2528025afd17ea3d26fd0c244949f73d573a
Rob <rob@kbamarketing.co.uk>
2020-12-02 14:10:43 +0000

    Get scss working

---
+++ scripts/application/application.js
@@ -0,0 +1,26 @@
+/*!---------------------------------------------------------------------------*\
+    Project Name
+
+    - Version:      1.0
+    - Author:       YOUR NAME
+    - Site:         http://YOUR_SITE
+    - Copyright:    2019, YOUR COMPANY
+    – License:      YOUR LICENSE
+\*----------------------------------------------------------------------------*/
+
+/* Imports */
+
+import NeueKit from '@neuekit/neuekit/src/scripts/neuekit';
+import * as util from './modules/utilities';
+
+
+/* Neue Kit */
+
+const neuekit = new NeueKit();
+
+window.neuekit = neuekit;
+/* Project Code */
+
+
+
+
---
+++ scripts/polyfills/polyfills.js
@@ -0,0 +1,16 @@
+/*----------------------------------------------------------------------------*\
+    #POLYFILLS
+
+    The features of tomorrow today! (Dont panic its only 6.58kb GZipped)
+\*----------------------------------------------------------------------------*/
+
+import 'custom-event-polyfill';     // IE11
+import 'element-closest';           // IE11
+import 'promise-polyfill';          // IE11
+import 'svg4everybody';             // IE11
+import 'whatwg-fetch';              // IE11
+
+import 'smoothscroll-polyfill';     // IE11, Edge, Safari, Android
+import 'stickyfilljs';              // IE11, Edge, Chrome, Opera, Android
+
+
