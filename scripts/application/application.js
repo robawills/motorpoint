e310d44cb6dab7c6eb720365ab8df627256a2d5a
robawills <robawills@me.com>
2021-08-24 13:56:49 +0100

    Update js

--- scripts/application/application.js
+++ scripts/application/application.js
@@ -1,10 +1,25 @@
-import owl from "./modules/owl.js";
-import aos from "./modules/aos.js";
+// import owl from "./modules/owl.js";
+// import aos from "./modules/aos.js";
 
 
-document.addEventListener("DOMContentLoaded", () => {
-  //  Always init
-  owl();
-  aos();
+// document.addEventListener("DOMContentLoaded", () => {
+//   //  Always init
+//   owl();
+//   aos();
   
+// });
+
+import $ from "jquery";
+window.jQuery = $;
+
+import owl from "./modules/owl.js";
+$(() => {
+  owl();
 });
+
+import aos from "./modules/aos.js";
+$(() => {
+  aos();
+});
+
+console.log("New version");
